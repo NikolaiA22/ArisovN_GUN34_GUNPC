0e06c44433b79059161c5e73cad003b1b00263a0
Nikolai Arisov <arisov124@gmail.com>
2024-12-23 19:04:29 +0400

    homework

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a, b;
+
+        Console.WriteLine("First num:");
+        if (!int.TryParse(Console.ReadLine(), out a))
+        {
+            Console.WriteLine("Error!");
+            return;
+        }
+
+        Console.WriteLine("Second num:");
+        if (!int.TryParse(Console.ReadLine(), out b))
+        {
+            Console.WriteLine("Error!");
+            return;
+        }
+
+        Console.WriteLine("Enter operator (&, |, ^):");
+        char operation = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        int result;
+        switch (operation)
+        {
+            case '&':
+                result = a & b;
+                break;
+            case '|':
+                result = a | b;
+                break;
+            case '^':
+                result = a ^ b;
+                break;
+            default:
+                Console.WriteLine("Error!");
+                return;
+        }
+
+        Console.WriteLine($"Result in decimal: {result}");
+        Console.WriteLine($"Result in binary: {Convert.ToString(result, 2)}");
+        Console.WriteLine($"Result in hexadecimal: {Convert.ToString(result, 16).ToUpper()}");
+    }
+}
